c25ed500d6687b3d46167e07494e30bad639c03a
Dang Nguyen Hai <dang.nguyen@orientsoftware.com>
2019-05-31 11:37:10 +0700

    add animation for each row of racer-list table

--- src/app/racer/racer-detail/racer-detail.component.spec.ts
+++ src/app/racer/racer-detail/racer-detail.component.spec.ts
@@ -1,16 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { RacerDetailComponent } from './racer-detail.component';
+import { RacerDetailComponent } from "./racer-detail.component";
 
-describe('RacerDetailComponent', () => {
+describe("RacerDetailComponent", () => {
   let component: RacerDetailComponent;
   let fixture: ComponentFixture<RacerDetailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RacerDetailComponent ]
-    })
-    .compileComponents();
+      declarations: [RacerDetailComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +18,7 @@ describe('RacerDetailComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
